f6b88eb5bc315d8d0291be3f7c4da9fb0ee63d38
Emily <emilyj4482@gmail.com>
2025-05-27 23:06:19 +0900

    [chore] create project

---
+++ GridPopGame/GridPopGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridPopGameApp.swift
+//  GridPopGame
+//
+//  Created by EMILY on 27/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct GridPopGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
