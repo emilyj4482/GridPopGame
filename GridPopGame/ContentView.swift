8982ea09d83a86d67ba62ab221a096a53c61db6d
Emily <emilyj4482@gmail.com>
2025-05-29 01:17:26 +0900

    [feat] items added

--- GridPopGame/ContentView.swift
+++ GridPopGame/ContentView.swift
@@ -7,9 +7,19 @@
 
 import SwiftUI
 import SpriteKit
+import GameplayKit
 
 class GameScene: SKScene {
+    
+    var columns: [[Item]] = []
+    var itemSize: CGFloat = 50
+    var itemsPerColumn: Int = 7
+    var itemsPerRow: Int = 7
+    
     override func didMove(to view: SKView) {
+        // set scene size explicitly
+        self.size = view.bounds.size
+        
         // create background sprite
         let background = SKSpriteNode(imageNamed: "background")
         
@@ -18,6 +28,48 @@ class GameScene: SKScene {
         background.size = size      // scale to scene size
         
         addChild(background)
+        
+        for x in 0..<itemsPerRow {
+            var column = [Item]()
+            
+            for y in 0..<itemsPerColumn {
+                let item = createItem(column: x, row: y)
+                column.append(item)
+            }
+            
+            columns.append(column)
+        }
+    }
+    
+    func positionItem(for item: Item) -> CGPoint {
+        let xOffset: CGFloat = 50
+        let yOffset: CGFloat = 300
+        
+        let x = xOffset + itemSize * CGFloat(item.column)
+        let y = yOffset + itemSize * CGFloat(item.row)
+        
+        return CGPoint(x: x, y: y)
+    }
+    
+    func createItem(column: Int, row: Int, startOffScreen: Bool = false) -> Item {
+        let itemImages = [
+            "cheese",
+            "mackerel",
+            "gray",
+        ]
+        
+        let itemImage = itemImages[GKRandomSource.sharedRandom().nextInt(upperBound: itemImages.count)]
+        
+        let item = Item(imageNamed: itemImage)
+        
+        item.name = itemImage
+        item.column = column
+        item.row = row
+        item.position = positionItem(for: item)
+        item.size = CGSize(width: itemSize, height: itemSize)
+        addChild(item)
+        
+        return item
     }
 }
 
---
+++ GridPopGame/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  GridPopGame
+//
+//  Created by EMILY on 28/05/2025.
+//
+
+import Foundation
+import SpriteKit
+
+class Item: SKSpriteNode {
+    var column: Int = -1
+    var row: Int = -1
+}
